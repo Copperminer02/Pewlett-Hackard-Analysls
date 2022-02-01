81921e7519d3f5833828ccd9f5128f4512ce847b
Copperminer02 <91850824+Copperminer02@users.noreply.github.com>
2022-01-31 18:20:00 -0700

    Added additional files and challenge code

---
+++ Employee_Database_Challenge.sql
@@ -0,0 +1,205 @@
+-- Create new table to hold current employee names and titles due to retire
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	t.title,
+	t.from_date,
+	t.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as t
+ON e.emp_no = t.emp_no
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+-- Review retirement_titles before exporting as csv retirement_titles.csv
+SELECT * FROM retirement_titles;
+
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+	first_name,
+	last_name,
+	title
+INTO unique_titles
+FROM retirement_titles
+WHERE to_date = '9999-01-01'
+ORDER BY emp_no, to_date DESC;
+
+-- Review unique_titles before exporting as csv retirement_titles.csv
+SELECT * FROM unique_titles;
+
+-- Count the number of potential retirees by current job title
+SELECT COUNT(title),
+	title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY count DESC;
+
+-- Review retiring_titles before exporting as csv retirement_titles.csv
+SELECT * FROM retiring_titles;
+
+-- Querry the employees available for mentorship programs born between January 1, 1965 and December 31,1965
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO mentorship_eliginility
+FROM employees as e
+INNER JOIN dept_emp as de
+ON e.emp_no = de.emp_no
+INNER JOIN titles as t
+ON de.emp_no = t.emp_no
+WHERE (de.to_date = '9999-01-01')
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
+
+
+-- EXTRA Tables for Module Challenge
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
+-- Determine how many employees, what department, and what position are ready for retirement
+SELECT DISTINCT ON(u.emp_no) u.emp_no,
+	u.title,
+	de.dept_no,
+	d.dept_name
+INTO retirees_by_title_dept
+FROM unique_titles AS u
+INNER JOIN dept_emp as de
+ON u.emp_no = de.emp_no
+INNER JOIN departments AS d
+ON de.dept_no = d.dept_no
+GROUP BY u.emp_no,u.title,de.dept_no,d.dept_name;
+
+SELECT * FROM retirees_by_title_dept;
+
+SELECT r.dept_name,
+	r.title,
+	COUNT(title)
+INTO titles_by_dept_retirees
+FROM retirees_by_title_dept AS r
+GROUP BY r.dept_name,r.title;	
+
+SELECT * FROM titles_by_dept_retirees;
+	
+-- Count the number of potential mentorees by current job title
+SELECT COUNT(title),
+	title
+INTO mentorship_titles
+FROM mentorship_eliginility
+GROUP BY title
+ORDER BY count DESC;
+
+SELECT * FROM mentorship_titles;
+
+-- Sort mentorship candidates by department and title
+SELECT DISTINCT ON(me.emp_no) me.emp_no,
+	me.title,
+	de.dept_no,
+	d.dept_name
+INTO mentorees_by_title_dept
+FROM mentorship_eliginility AS me
+INNER JOIN dept_emp as de
+ON me.emp_no = de.emp_no
+INNER JOIN departments AS d
+ON de.dept_no = d.dept_no
+GROUP BY me.emp_no,me.title,de.dept_no,d.dept_name;
+
+
+SELECT mt.dept_name,
+	mt.title,
+	COUNT(title)
+INTO titles_by_dept_mentors
+FROM mentorees_by_title_dept AS mt
+GROUP BY mt.dept_name,mt.title;	
+
+
+SELECT * FROM titles_by_dept_mentors;
+
+-- Find total current employees in employees file
+SELECT DISTINCT ON(e.emp_no)e.emp_no,
+	e.birth_date,
+	t.title,
+	de.dept_no,
+	d.dept_name
+INTO employee_current
+FROM employees AS e
+INNER JOIN dept_emp as de
+ON e.emp_no = de.emp_no
+INNER JOIN titles AS t
+ON e.emp_no = t.emp_no
+INNER JOIN departments AS d
+ON de.dept_no  = d.dept_no
+WHERE (de.to_date = '9999-01-01')
+ORDER BY e.emp_no, de.to_date DESC;
+
+SELECT * FROM employee_current;
+
+-- Sort all current employees by title
+SELECT COUNT(title),
+	title
+INTO all_titles
+FROM employee_current
+GROUP BY title
+ORDER BY count DESC;
+
+SELECT * FROM all_titles;
+
+-- Get all current employees with by title and department
+SELECT DISTINCT ON(ec.emp_no) ec.emp_no,
+	ec.title,
+	de.dept_no,
+	d.dept_name
+INTO current_by_title_dept
+FROM employee_current AS ec
+INNER JOIN dept_emp as de
+ON ec.emp_no = de.emp_no
+INNER JOIN departments AS d
+ON de.dept_no = d.dept_no
+ORDER BY ec.emp_no;
+
+SELECT * FROM current_by_title_dept;
+
+-- Sort all current employees by by totals by department and by title
+SELECT ct.dept_name,
+	ct.title,
+	COUNT(title)
+INTO titles_by_dept_current
+FROM current_by_title_dept AS ct
+GROUP BY ct.dept_name,ct.title;	
+
+SELECT * FROM titles_by_dept_current;
+
+-- Merge all tables by that sort employees by titles only
+SELECT r.title,
+	a.count AS all_employees,
+	r.count AS retiring_employees,
+	m.count AS mentor_eligible
+INTO titles_mergred
+FROM retiring_titles AS r
+LEFT JOIN mentorees_by_title_dept AS m
+ON r.title = m.title
+LEFT JOIN all_titles AS a
+ON r.title = a.title
+GROUP BY r.title,a.count,r.count
+ORDER BY r.title ASC;
+
+-- Merge all tables that combine all employees by title and by deparment
+
+SELECT c.dept_name,
+	c.title,
+	c.count AS all_employees,
+	r.count AS retiring_employees,
+	m.count AS mentor_eligible
+INTO titles_dept_merged
+FROM titles_by_dept_current AS c
+LEFT JOIN titles_by_dept_retirees AS r
+ON c.dept_name = r.dept_name
+AND c.title = r.title
+LEFT JOIN titles_by_dept_mentors AS m
+ON c.dept_name = m.dept_name
+AND c.title = m.title
+ORDER BY c.dept_name ASC;
