f2a9759dd858b8879a4661c382938f24d92f3cc6
Copperminer02 <91850824+Copperminer02@users.noreply.github.com>
2022-02-03 21:12:46 -0700

    Add files via upload

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -1,3 +1,6 @@
+-- Drop Table if it does not exist
+DROP TABLE IF EXISTS retirement_titles;
+
 -- Create new table to hold current employee names and titles due to retire
 SELECT e.emp_no,
 	e.first_name,
@@ -15,6 +18,8 @@ ORDER BY e.emp_no;
 -- Review retirement_titles before exporting as csv retirement_titles.csv
 SELECT * FROM retirement_titles;
 
+-- Drop Table if it does not exist
+DROP TABLE IF EXISTS unique_titles;
 
 -- Use Dictinct with Orderby to remove duplicate rows
 SELECT DISTINCT ON (emp_no) emp_no,
@@ -29,6 +34,9 @@ ORDER BY emp_no, to_date DESC;
 -- Review unique_titles before exporting as csv retirement_titles.csv
 SELECT * FROM unique_titles;
 
+-- Drop Table if it does not exist
+DROP TABLE IF EXISTS retiring_titles;
+
 -- Count the number of potential retirees by current job title
 SELECT COUNT(title),
 	title
@@ -40,28 +48,54 @@ ORDER BY count DESC;
 -- Review retiring_titles before exporting as csv retirement_titles.csv
 SELECT * FROM retiring_titles;
 
--- Querry the employees available for mentorship programs born between January 1, 1965 and December 31,1965
-SELECT DISTINCT ON (e.emp_no) e.emp_no,
+-- PART 2 Challenge
+
+-- Drop Table if it does not exist
+DROP TABLE IF EXISTS mentor_titles;
+
+-- Create new table to hold current employee names and titles in mentor category
+SELECT e.emp_no,
 	e.first_name,
 	e.last_name,
 	e.birth_date,
-	de.from_date,
-	de.to_date,
+	t.from_date,
+	t.to_date,
 	t.title
-INTO mentorship_eliginility
+INTO mentor_titles
 FROM employees as e
-INNER JOIN dept_emp as de
-ON e.emp_no = de.emp_no
 INNER JOIN titles as t
-ON de.emp_no = t.emp_no
-WHERE (de.to_date = '9999-01-01')
-AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ON e.emp_no = t.emp_no
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
 ORDER BY e.emp_no;
 
+-- Review retirement_titles
+SELECT * FROM mentor_titles;
+
+-- Drop Table if it does not exist
+DROP TABLE IF EXISTS mentorship_eliginility;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+	first_name,
+	last_name,
+	birth_date,
+	from_date,
+	to_date,
+	title
+INTO mentorship_eliginility
+FROM mentor_titles
+WHERE to_date = '9999-01-01'
+ORDER BY emp_no, to_date DESC;
+
+-- Review mentorship_eliginility before exporting as csv retirement_titles.csv
 SELECT * FROM mentorship_eliginility;
--- EXTRA Tables for Module Challenge
+
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- 
+-- Drop Table if it does not exist
+DROP TABLE IF EXISTS retirees_by_title_dept;
+
 -- Determine how many employees, what department, and what position are ready for retirement
+-- group retirees and titles to deprtment name
 SELECT DISTINCT ON(u.emp_no) u.emp_no,
 	u.title,
 	de.dept_no,
@@ -76,6 +110,10 @@ GROUP BY u.emp_no,u.title,de.dept_no,d.dept_name;
 
 SELECT * FROM retirees_by_title_dept;
 
+-- Drop Table if it does not exist
+DROP TABLE IF EXISTS titles_by_dept_retirees;
+
+-- count titles for retiree group by department
 SELECT r.dept_name,
 	r.title,
 	COUNT(title)
@@ -84,7 +122,11 @@ FROM retirees_by_title_dept AS r
 GROUP BY r.dept_name,r.title;	
 
 SELECT * FROM titles_by_dept_retirees;
-	
+
+
+-- Drop Table if it does not exist
+DROP TABLE IF EXISTS mentorship_titles;
+
 -- Count the number of potential mentorees by current job title
 SELECT COUNT(title),
 	title
@@ -94,6 +136,9 @@ GROUP BY title
 ORDER BY count DESC;
 
 SELECT * FROM mentorship_titles;
+
+-- Drop Table if it does not exist
+DROP TABLE IF EXISTS mentorees_by_title_dept;
 
 -- Sort mentorship candidates by department and title
 SELECT DISTINCT ON(me.emp_no) me.emp_no,
@@ -108,7 +153,10 @@ INNER JOIN departments AS d
 ON de.dept_no = d.dept_no
 GROUP BY me.emp_no,me.title,de.dept_no,d.dept_name;
 
+-- Drop Table if it does not exist
+DROP TABLE IF EXISTS titles_by_dept_mentors;
 
+-- group titles by department name 
 SELECT mt.dept_name,
 	mt.title,
 	COUNT(title)
@@ -119,35 +167,47 @@ GROUP BY mt.dept_name,mt.title;
 
 SELECT * FROM titles_by_dept_mentors;
 
+-- Drop Table if it does not exist
+DROP TABLE IF EXISTS current_titles;
+
 -- Find total current employees in employees file
 SELECT e.emp_no,
-	t.title,
-	de.to_date,
-	de.dept_no
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	t.from_date,
+	t.to_date,
+	t.title
 INTO current_titles
-FROM employees AS e
-JOIN dept_emp as de
-ON e.emp_no = de.emp_no
-JOIN titles AS t
+FROM employees as e
+LEFT JOIN titles as t
 ON e.emp_no = t.emp_no
+ORDER BY e.emp_no;
 
+-- Review current_titles
+SELECT * FROM current_titles;
 
-
+-- Drop Table if it does not exist
+DROP TABLE IF EXISTS unique_current_titles;
 
 -- Find total current unique employees in employees file
 SELECT DISTINCT ON (c.emp_no) c.emp_no,
 	c.title,
-	c.dept_no,
+	de.dept_no,
 	d.dept_name
 INTO unique_current_titles
 FROM current_titles AS c
+INNER JOIN dept_emp as de
+ON c.emp_no = de.emp_no
 INNER JOIN departments AS d
-ON c.dept_no = d.dept_no
-WHERE to_date = '9999-01-01'
-ORDER BY emp_no, to_date DESC;
+ON de.dept_no = d.dept_no
+WHERE c.to_date = '9999-01-01'
+ORDER BY c.emp_no, c.to_date DESC;
 
 SELECT * FROM unique_current_titles;
 
+-- Drop Table if it does not exist
+DROP TABLE IF EXISTS all_titles;
 
 -- Sort all current employees by title
 SELECT COUNT(title),
@@ -159,15 +219,39 @@ ORDER BY count DESC;
 
 SELECT * FROM all_titles;
 
--- Sort all current employees by by totals by department and by title
-SELECT dept_name,
-	title,
+
+-- Drop Table if it does not exist
+DROP TABLE IF EXISTS current_by_title_dept;
+
+SELECT DISTINCT ON(u.emp_no) u.emp_no,
+	u.title,
+	de.dept_no,
+	d.dept_name
+INTO current_by_title_dept
+FROM unique_current_titles AS u
+LEFT JOIN dept_emp as de
+ON u.emp_no = de.emp_no
+LEFT JOIN departments AS d
+ON de.dept_no = d.dept_no
+GROUP BY u.emp_no,u.title,de.dept_no,d.dept_name;
+
+SELECT * FROM current_by_title_dept;
+
+-- Drop Table if it does not exist
+DROP TABLE IF EXISTS titles_by_dept_current;
+
+-- count titles for retiree group by department
+SELECT c.dept_name,
+	c.title,
 	COUNT(title)
 INTO titles_by_dept_current
-FROM unique_current_titles AS u
-GROUP BY dept_name,title;	
+FROM current_by_title_dept AS c
+GROUP BY c.dept_name,c.title;	
 
 SELECT * FROM titles_by_dept_current;
+
+-- Drop table if exists
+DROP TABLE IF EXISTS titles_mergred;
 
 -- Merge all tables by that sort employees by titles only
 SELECT r.title,
@@ -184,13 +268,17 @@ GROUP BY r.title,a.count,r.count
 ORDER BY r.title ASC;
 
 SELECT * FROM titles_mergred;
+
+-- Drop Table if it does not exist
+DROP TABLE IF EXISTS titles_dept_merged;
+
 -- Merge all tables that combine all employees by title and by deparment
 
 SELECT c.dept_name,
 	c.title,
-	c.count AS all_employees,
-	r.count AS retiring_employees,
-	m.count AS mentor_eligible
+	c.count AS "all_employees",
+	r.count AS "retiring_employees",
+	m.count AS "mentor_eligible"
 INTO titles_dept_merged
 FROM titles_by_dept_current AS c
 LEFT JOIN titles_by_dept_retirees AS r
